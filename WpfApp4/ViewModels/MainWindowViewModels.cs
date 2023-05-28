7fdf15e481cb98f0be8852429421c4f4b2384e39
RasulSz <sebziyevresul3@gmail.com>
2023-05-28 14:29:49 +0400

    qwraaf

--- WpfApp4/ViewModels/MainWindowViewModels.cs
+++ WpfApp4/ViewModels/MainWindowViewModels.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using WpfApp4.Commands;
 
 namespace WpfApp4.ViewModels
@@ -60,7 +64,40 @@ namespace WpfApp4.ViewModels
 
             ShowAllCommand = new RelayCommand(obj =>
             {
-
+                string cs = "";
+                DataTable table;
+                SqlDataReader reader;
+                using (var conn = new SqlConnection())
+                {
+                    conn.ConnectionString = cs;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "SELECT * FROM Authors";
+                    command.Connection = conn;
+                    table = new DataTable();
+                    bool hasColumnAdded = false;
+                    using (reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (!hasColumnAdded)
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    table.Columns.Add(reader.GetName(i));
+                                }
+                                hasColumnAdded = true;
+                            }
+                            DataRow row = table.NewRow();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[i] = reader[i];
+                            }
+                            table.Rows.Add(row);
+                        }
+                        author.ItemsSource = table.DefaultView;
+                    }
+                }
             });
         }
     }
